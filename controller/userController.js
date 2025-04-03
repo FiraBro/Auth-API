52f229bd8013848aa068436aad33e13ae4d539cd
Firagos <jemalfiragos@gmail.com>
2025-04-03 09:01:48 +0300

    I create createUser in which is the final task for this controller for now

--- controller/userController.js
+++ controller/userController.js
@@ -38,7 +38,7 @@ exports.createUser = async (req, res) => {
       name: req.body.name,
       password: req.body.password,
       passwordConfirm: req.body.passwordConfirm,
-      email:req.body.email,
+      email: req.body.email,
       role: req.body.role,
     });
     res.status(200).json({
@@ -46,6 +46,26 @@ exports.createUser = async (req, res) => {
       data: {
         user: user,
       },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+exports.deleteUser = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Incorrect ID",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      message: "User succusfully deleted",
     });
   } catch (error) {
     res.status(404).json({
