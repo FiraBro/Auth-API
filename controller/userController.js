a128e0a5636efd4747c33cd5fb7ad0d8760ebb59
Firagos <jemalfiragos@gmail.com>
2025-04-03 10:02:15 +0300

    i fix the bug

--- controller/userController.js
+++ controller/userController.js
@@ -32,28 +32,6 @@ exports.getUser = async (req, res) => {
     });
   }
 };
-exports.createUser = async (req, res) => {
-  try {
-    const user = await User.create({
-      name: req.body.name,
-      password: req.body.password,
-      passwordConfirm: req.body.passwordConfirm,
-      email: req.body.email,
-      role: req.body.role,
-    });
-    res.status(200).json({
-      status: "success",
-      data: {
-        user: user,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "fail",
-      message: error,
-    });
-  }
-};
 exports.deleteUser = async (req, res) => {
   try {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
