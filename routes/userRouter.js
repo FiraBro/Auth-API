04aa59dbc9f9296d913e0002bc4543565e7d237f
Firagos <jemalfiragos@gmail.com>
2025-04-03 10:06:37 +0300

    adding the signup routes to userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const userRouter = express.Router();
 const userController = require("../controller/userController");
+const authController = require("../controller/authController");
+
+// Fix: Reference the specific `signup` function from `authController`
+userRouter.post("/signup", authController.register);
+
+// Other routes
 userRouter.route("/").get(userController.getAllUser);
+userRouter.route("/:id").delete(userController.deleteUser);
+
 module.exports = userRouter;
