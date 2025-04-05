fc037695b591737b587938aec2a326be17fa0bc2
Firagos <jemalfiragos@gmail.com>
2025-04-05 17:07:30 +0300

    i rewrite all of the line

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 // const path = require("path"); // Add path module
+// const multer = require("multer");
+
 app.use(cors());
 // Load environment variables
 dotenv.config({ path: "config.env" });
@@ -11,6 +13,12 @@ dotenv.config({ path: "config.env" });
 // Middleware
 app.use(express.json());
 app.use(morgan("dev"));
+
+// Serve static files from uploads directory
+
 const userRouter = require("./routes/userRouter");
+const courseRouter = require("./routes/courseRouter");
 app.use("/api/v2/users", userRouter);
+app.use("/api/v2/course", courseRouter);
+
 module.exports = app;
