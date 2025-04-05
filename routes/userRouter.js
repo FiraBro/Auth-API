bf6494a7006b54cf066a0076d4c03168c60de386
Firagos <jemalfiragos@gmail.com>
2025-04-05 17:16:22 +0300

    i added login route to userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ const authController = require("../controller/authController");
 
 // Fix: Reference the specific `signup` function from `authController`
 userRouter.post("/signup", authController.register);
-
+userRouter.post('/login',authController.login)
 // Other routes
 userRouter.route("/").get(userController.getAllUser);
 userRouter.route("/:id").delete(userController.deleteUser);
