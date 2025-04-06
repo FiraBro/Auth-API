8aeb3b272e35c7bf1f53ce80a016edc720bd99ed
Firagos <jemalfiragos@gmail.com>
2025-04-06 09:15:40 +0300

    i update userController to allow user inorder to update their email and some function

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
 
 exports.getAllUser = async (req, res) => {
   try {
@@ -32,6 +33,34 @@ exports.getUser = async (req, res) => {
     });
   }
 };
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use /updateMyPassword.",
+        400
+      )
+    );
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, "name", "email");
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.deleteUser = async (req, res) => {
   try {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
