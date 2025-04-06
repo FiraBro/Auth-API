9e08c6739cb343ff3399e69c0b025d903f9e9fee
Firagos <jemalfiragos@gmail.com>
2025-04-06 09:31:57 +0300

    i changed service from mailtrap to gmail

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,26 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const sendEmail = async options => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
-    }
-  });
+const sendEmail = async (options) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "jemalfiragos@gmail.com",
+        pass: "giesvnlouvsnquib", // Use the generated App Password
+      },
+    });
+    const mailOptions = {
+      from: "Firagos Jemal <jemalfiragos@gmail.com>",
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: 'Firagos Jemal <jemalfiragos@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
-
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Failed to send email. Please try again later.");
+  }
 };
 
-module.exports = sendEmail;
+module.exports = { sendEmail }; // Export as an object
